update wrong value in return obs

sheeprl/envs/dummy.py
@@ -43,7 +43,7 @@ class BaseDummyEnv(gym.Env, ABC):
         if self._dict_obs_space:
             return {
                 # da sostituire con np.random.rand
-                "rgb": np.full(self.observation_space["rgb"].shape, self._current_step % 256, dtype=np.uint8),
+                "rgb": np.full(self.observation_space["rgb"].shape, , dtype=np.uint8),
                 "state": np.full(self.observation_space["state"].shape, self._current_step, dtype=np.uint8),
             }
         else:

sheeprl/envs/tnmr.py
@@ -178,10 +178,10 @@ class TNMRGradEnv(gym.Env):
     def get_obs(self) -> Dict[str, np.ndarray]:
         if self._dict_obs_space:
             return {
-                "pulse": np.full(self.observation_space["pulse"].shape, self._current_step, dtype=np.float32),
+                "pulse": np.full(self.observation_space["pulse"].shape, self.preemphasized_waveform, dtype=np.float32),
             }
         else:
-            return np.full(self.observation_space.shape, self._current_step, dtype=np.float32)
+            return np.full(self.observation_space.shape, self.preemphasized_waveform, dtype=np.float32)
 
     
 