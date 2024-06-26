2c26f8ab4c65e78c32ecf3717a14aca0f33d8c09
jonbmartin <jon.bach.martin@gmail.com>
2024-06-26 09:57:04 -0500

    squeeze

--- sheeprl/algos/dreamer_v3/utils.py
+++ sheeprl/algos/dreamer_v3/utils.py
@@ -86,7 +86,7 @@ def prepare_obs(
         print('debugging!!!!')
         print(np.shape(test))
         print(test.dtype)
-        torch_obs[k] = torch.from_numpy(v.copy()).to(fabric.device).float()
+        torch_obs[k] = torch.from_numpy(np.squeeze(v.copy())).to(fabric.device).float()
         if k in cnn_keys:
             torch_obs[k] = torch_obs[k].view(1, num_envs, -1, *v.shape[-2:]) / 255 - 0.5
         else:
