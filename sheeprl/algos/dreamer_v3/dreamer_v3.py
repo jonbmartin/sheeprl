930523c32a86d66625bf909e8b7b23e27372c4bb
jonbmartin <jon.bach.martin@gmail.com>
2024-07-16 08:58:34 -0500

    undo all changes to dreamer_v3

--- sheeprl/algos/dreamer_v3/dreamer_v3.py
+++ sheeprl/algos/dreamer_v3/dreamer_v3.py
@@ -536,7 +536,6 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
         )
 
     # Get the first environment observation and start the optimization
-    all_episode_step_data = []
     step_data = {}
     obs = envs.reset(seed=cfg.seed)[0]
     for k in obs_keys:
@@ -585,8 +584,7 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
                         )
 
                 step_data["actions"] = actions.reshape((1, cfg.env.num_envs, -1))
-                all_episode_step_data.append(step_data)
-                rb.add(all_episode_step_data[-1], validate_args=cfg.buffer.validate_args)
+                rb.add(step_data, validate_args=cfg.buffer.validate_args)
 
                 next_obs, rewards, terminated, truncated, infos = envs.step(
                     real_actions.reshape(envs.action_space.shape)
@@ -649,10 +647,9 @@ def main(fabric: Fabric, cfg: Dict[str, Any]):
                 reset_data["actions"] = np.zeros((1, reset_envs, np.sum(actions_dim)))
                 reset_data["rewards"] = step_data["rewards"][:, dones_idxes]
                 reset_data["is_first"] = np.zeros_like(reset_data["terminated"])
-                # TODO: here, add all data since last reset to RB 
                 rb.add(reset_data, dones_idxes, validate_args=cfg.buffer.validate_args)
 
-                # Reset already inserted step datarb
+                # Reset already inserted step data
                 step_data["rewards"][:, dones_idxes] = np.zeros_like(reset_data["rewards"])
                 step_data["terminated"][:, dones_idxes] = np.zeros_like(step_data["terminated"][:, dones_idxes])
                 step_data["truncated"][:, dones_idxes] = np.zeros_like(step_data["truncated"][:, dones_idxes])
