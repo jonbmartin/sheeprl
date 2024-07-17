0c3ca88aef28f35b25e593097fe20a364474c224
jonbmartin <jon.bach.martin@gmail.com>
2024-07-17 11:26:26 -0500

    not using reward_v for reward, fixed

--- sheeprl/envs/tnmr.py
+++ sheeprl/envs/tnmr.py
@@ -103,9 +103,8 @@ class TNMRGradEnv(gym.Env):
             # reward is the cost up to the end of the window: 
             print('Done measuring on TNMR!')
             reward_v = - np.abs(error_v[(self._current_step-self.measure_interval):self._current_step]**2)
-            print('REWARD V SIZE')
-            print(np.size(reward_v))
-            reward = - np.sum(np.abs(error_v**2))/self.measure_interval
+            reward = np.sum(reward_v)
+            #reward = - np.sum(np.abs(error_v**2))/self.measure_interval
             print(f'REWARD ={reward}')
         else:
             reward = 0
