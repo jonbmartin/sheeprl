d1975d930ae949e61d072ceef73e54e5c9418ddf
jonbmartin <jon.bach.martin@gmail.com>
2024-07-24 11:32:55 -0500

    tweak some experiment settings

--- sheeprl/envs/tnmr.py
+++ sheeprl/envs/tnmr.py
@@ -43,10 +43,10 @@ class TNMRGradEnv(gym.Env):
         self._n_steps = self.ideal_waveform.size
         self._current_step = 0
         self._n_grad_measurement_averages = 1.0
-        self.action_scale = 35
+        self.action_scale = 30
 
         # this determines how often you actually collect data on TNMR
-        self.measure_interval = 50
+        self.measure_interval = 60
         self.BASE_COST = 10
 
         # info for communicating to the TNMR magnet
